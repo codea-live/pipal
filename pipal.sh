f02eedb279d75dc3c3e29a95a9c4a6bea5d6a626
ADAMJR <adamjf123@gmail.com>
2021-06-06 23:39:53 +0100

    add working script to update csv

--- pipal.sh
+++ pipal.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-while IFS=, read -r name code; do
-  echo "$name"
-done < $DATESTAMP.csv
+CPU=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')
+MEMORY=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+STORAGE=$(df -h --total | grep total | awk '{print $5}')
+
+LONG_TEMP=$(cat /sys/class/thermal/thermal_zone*/temp)
+TEMP=$(expr $LONG_TEMP / 1000)
+
+DATE=$(date -I)
+TIME=$(date +%H:%M)
+LOG_PATH="log/$DATE.csv"
+
+if [ ! -s $LOG_PATH ]; then
+  echo time,cpu,memory,storage,temp > $LOG_PATH
+fi
+
+echo $TIME,$CPU,$MEMORY,$STORAGE,$TEMP >> $LOG_PATH
