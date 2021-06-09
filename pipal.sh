69b746d34fb8690845bf85f3ef2fb067e9bb9bc4
ADAMJR <adamjf123@gmail.com>
2021-06-09 14:48:13 +0100

    Now With Real Data!

--- pipal.sh
+++ pipal.sh
@@ -2,10 +2,10 @@
 cd $(dirname $0)
 sh setup.sh
 
-CPU="3"
-MEMORY="42"
-STORAGE="8.6"
-TEMP="43.5"
+CPU=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')
+MEMORY=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+STORAGE=$(df / | awk 'END{print $5}' | sed -e 's/[%]//g')
+TEMP=$(cat /sys/class/thermal/thermal_zone*/temp | xargs -i expr {} / 1000)
 TIME=$(date +"%D %T")
 
 LOG_PATH="website/log.csv"
