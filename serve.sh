efa2004adff06bb1ba9a4917831fd9a094cff5b7
ADAMJR <adamjf123@gmail.com>
2021-07-07 18:15:12 +0100

    Serve PiPal Website

---
+++ serve.sh
@@ -0,0 +1,2 @@
+cd $(dirname $0)
+python3 -m http.server 8080 -d website
---
+++ setup.sh
@@ -0,0 +1,6 @@
+INTERVAL_MINS=1
+LOG_JOB="*/$INTERVAL_MINS * * * * sh $(pwd)/pipal.sh"
+START_JOB="@reboot sh $(pwd)/serve.sh"
+
+(crontab -l | grep -Fv "$LOG_JOB" ; echo "$LOG_JOB") | crontab -
+(crontab -l | grep -Fv "$START_JOB" ; echo "$START_JOB") | crontab -
